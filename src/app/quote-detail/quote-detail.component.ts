b68777da7226f41834719f0f7b5ca3597a2e08aa
eude313 <eudes.charles@student.moringaschool.com>
2021-07-20 19:03:20 +0300

    add modifications

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -20,8 +20,8 @@ export class QuoteDetailComponent implements OnInit {
   
   bless:Quote[]=[
     new Quote("Carbon", "Be comfortable", new Date ( 2000,11,23), "“Write it. Shoot it. Publish it. Crochet it, sauté it, whatever. MAKE.” "),
-    new Quote("master", "Be comfortable", new Date ( 2011,11,2), "“Write it. Shoot it. Publish it. Crochet it, sauté it, whatever. MAKE.” "),
-    new Quote("mio", "Be comfortable", new Date ( 2011,11,2), "“Write it. Shoot it. Publish it. Crochet it, sauté it, whatever. MAKE.” "),
+    new Quote("master", "Education Quotes", new Date ( 2011,11,2), "Intelligence plus character – that is the goal of true education."),
+    new Quote("mio", "Doubt Quotes", new Date ( 2011,11,2), " To rest upon a formula is a slumber that, prolonged, means death."),
     
   ];
 
